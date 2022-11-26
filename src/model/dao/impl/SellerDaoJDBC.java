0d4fe872bd827c06e8de6aad49ce19ca52536196
diegofelipe25 <diego_felipe25@outlook.com>
2022-11-26 10:50:11 -0300

    conserto dao

--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -72,15 +72,13 @@ public class SellerDaoJDBC implements SellerDao {
 			st = conn.prepareStatement(
 					"UPDATE seller "
 					+ "SET Name = ?, Email = ?, BirthDate = ?, BaseSalary = ?, DepartmentId = ? "
-					+ "WHERE Id = ?");
-			
+					+ "WHERE Id = ?");		
 			st.setString(1, obj.getName());
 			st.setString(2, obj.getEmail());
 			st.setDate(3, new java.sql.Date(obj.getBirthDate().getTime()));
 			st.setDouble(4, obj.getBaseSalary());
 			st.setInt(5, obj.getDepartment().getId());
-			st.setInt(6, obj.getId());
-			
+			st.setInt(6, obj.getId());		
 			st.executeUpdate();
 		}
 		catch (SQLException e) {
@@ -89,17 +87,13 @@ public class SellerDaoJDBC implements SellerDao {
 		finally {
 			DB.closeStatement(st);
 		}
-
 	}
-
 	@Override
 	public void deleteById(Integer id) {
 		PreparedStatement st = null;
 		try {
-			st = conn.prepareStatement("DELETE FROM seller WHERE Id = ?");
-			
+			st = conn.prepareStatement("DELETE FROM seller WHERE Id = ?");	
 			st.setInt(1, id);
-			
 			st.executeUpdate();
 		}
 		catch (SQLException e) {
@@ -108,9 +102,7 @@ public class SellerDaoJDBC implements SellerDao {
 		finally {
 			DB.closeStatement(st);
 		}
-
 	}
-
 	@Override
 	public Seller findById(Integer id) {
 		PreparedStatement st = null;
